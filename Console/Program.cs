ce52bcb7f2a4b835d0522189dd7920b713663519
Seifer <seed-seifer@ya.ru>
2013-03-16 22:48:54 +0530

    Initial commit

---
+++ Console/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using PortableLibrary;
+
+namespace Console
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			new MyClass().WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ PortableLibrary/MyClass.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace PortableLibrary
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+
+		public void WriteLine(string message)
+		{
+			Console.WriteLine (message);
+		}
+	}
+}
+
